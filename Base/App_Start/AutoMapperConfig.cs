c1d67568ae7d35b07987a4493a3d70f7ce75a9e1
lagoutx <scottalexanderco@gmail.com>
2016-12-14 22:22:45 +0000

    Base set up wth swagger and automapper

---
+++ Base/App_Start/AutoMapperConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.Owin.Security.OAuth;
+using Newtonsoft.Json.Serialization;
+
+namespace Base
+{
+    /// <summary>
+    /// Automapper set up
+    /// </summary>
+    public static class AutoMapperConfig
+    {
+        // Register maps
+        public static void Register()
+        {
+            
+        }
+    }
+}
