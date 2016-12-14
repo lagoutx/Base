ac8b238a960290ea6ac4d4a9a1efafc1ad45ac77
lagoutx <scottalexanderco@gmail.com>
2016-12-14 22:53:24 +0000

    Simple Injector added and configured for base and services

---
+++ Base/App_Start/DependencyConfig.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.Owin.Security.OAuth;
+using Newtonsoft.Json.Serialization;
+using SimpleInjector;
+using System.Web.Mvc;
+using SimpleInjector.Integration.Web.Mvc;
+using Base.Services.Services;
+
+namespace Base
+{
+    /// <summary>
+    /// Simple Injector Dependency Config
+    /// </summary>
+    public static class DependencyConfig
+    {
+
+        public static void Register()
+        {
+            // 1. Create a new Simple Injector container
+            var container = new Container();
+
+            // 2. Configure the container (register)
+            // See below for more configuration examples
+            InjectorSetup.Register(container);
+
+            // 3. Optionally verify the container's configuration.
+            container.Verify();
+
+            // 4. Store the container for use by the application
+            DependencyResolver.SetResolver(
+                new SimpleInjectorDependencyResolver(container));
+        }
+    }
+}
---
+++ Services/InjectionSetup.cs
@@ -0,0 +1,22 @@
+﻿using Base.Services.EF;
+using SimpleInjector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base.Services.Services
+{
+    public static class InjectorSetup
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="container"></param>
+        public static void Register(Container container)
+        {
+            container.Register<ITestService, TestService>(Lifestyle.Transient);
+        }
+    }
+}
